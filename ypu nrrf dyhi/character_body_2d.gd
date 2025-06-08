70ff3ec2dae633d60d5d1af3724f387c4632ad3b
Guru-CS <gurusaran.cs@gmail.com>
2025-06-08 16:37:11 -0400

    Delete ypu nrrf dyhi directory

--- ypu nrrf dyhi/character_body_2d.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-@onready var playe2r = get_node(".../CharacterBody2D")
-@onready var player = get_node("/root/GameJam/mc")
-func _physics_process(delta: float) -> void:
-	
-	
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 105.0
-	move_and_slide()
